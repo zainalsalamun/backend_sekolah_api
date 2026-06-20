b0dccc0d0007011c2cd128c8ee8edb2975faf970
zainalsalamun <zainalrtf@gmail.com>
2026-06-20 23:26:31 +0700

    feat(EbookModel): update fillable fields for enhanced ebook attributes

--- app/Models/Ebook.php
+++ app/Models/Ebook.php
@@ -2,11 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ebook extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'judul', 'deskripsi', 'penulis', 'sampul', 'file', 'tanggal',
+        'judul',
+        'pengarang',
+        'mata_pelajaran',
+        'deskripsi',
+        'gambar',
+        'file_url',
+        'halaman',
+        'ukuran',
+        'rating',
+        'jumlah_download',
+        'status',
     ];
 }
